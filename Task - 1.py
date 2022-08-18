d10bb99ef9d683420e97fed7bae82e65d2fbfe2d
Rasikabalasubramanian <110782682+Rasikabalasubramanian@users.noreply.github.com>
2022-08-18 12:05:13 -0400

    Task - 1

---
+++ Task - 1.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+Rasika = int(10)
+print(Rasika)
+
+
+# In[1]:
+
+
+Rasika = int10
+print(Rasika)
+
+
+# In[2]:
+
+
+Rasika ="int 10"
+print(Rasika)
+
+
+# In[3]:
+
+
+int = 10
+print(int)
+
+
+# In[6]:
+
+
+x=6
+y="rasika"
+print(x,y)
+
+
+# In[7]:
+
+
+x=30
+print(x)
+
+
+# In[12]:
+
+
+x=1
+y=2
+z=3
+print(x&y&z)
+
+
+# In[13]:
+
+
+x=1
+y=2
+z=3
+print(x,y,z)
+
+
+# In[15]:
+
+
+rasika="MIS"
+print(rasika)
+
+
+# In[18]:
+
+
+"""
+rasika="Mis"
+Mis= 10
+"""
+
+
+# In[21]:
+
+
+rasika="Mis"
+Mis= 10
+print(Mis)
+print(rasika)
+
+
+# In[22]:
+
+
+rasika="Mis"
+Mis= "10"
+print(Mis)
+print(rasika)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
