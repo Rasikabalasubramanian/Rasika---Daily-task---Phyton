66d7b94c7d3f922b61a0e15a459f26e42c7a6d5d
Rasikabalasubramanian <110782682+Rasikabalasubramanian@users.noreply.github.com>
2022-08-20 14:25:30 -0400

    Add files via upload

---
+++ R - Task 4 (20-8-2022).py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+x = "Information_Technology"
+y = "Python_Programming_Language"
+print(x[2:13])
+print(x[8:])
+print(x[:13])
+print(x[15:])
+print(x[1:21])
+print(x[5:])
+print(y[7:])
+print(y[2:24])
+print(y[1:7])
+print(y[12:19])
+print(y[9:15])
+print(y[16:25])
+print(y[9:])
+
