c4589d55e6ac5eb1db2a9a9b73ba2ac27278c873
Rasikabalasubramanian <110782682+Rasikabalasubramanian@users.noreply.github.com>
2022-08-20 18:49:56 -0400

    Add files via upload

---
+++ R - Task 1 (11-8-2022).py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+a = 1
+b = 4
+print(a)
+print(b)
+print(a,b)
+
+
+# In[7]:
+
+
+rasika = "Mis"
+Mis = "Rasika_Balasubramanian"
+print(rasika)
+print(Mis)
+
+
+# In[8]:
+
+
+a = "book","pen","note","pencil"
+print (a)
+
+
+# In[13]:
+
+
+x=y=z = "code"
+print(x)
+print(y)
+print(z)
+
+
+# In[18]:
+
+
+Fruits = ["Apple", "Orange", "Grapes"]
+x,y,z = Fruits
+print(Fruits)
+print(x)
+print(y)
+print(z)
+
+
+# In[21]:
+
+
+a = "love"
+b = "to feel"
+c = "humanity"
+print(a+b+c)
+print(a,b,c)
+
+
+# In[18]:
+
+
+"""
+rasika="Mis"
+Mis= 10
+"""
+
