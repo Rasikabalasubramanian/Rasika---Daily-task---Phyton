3a8824eba29ce099cb444c18b6423a8caab074c7
Rasikabalasubramanian <110782682+Rasikabalasubramanian@users.noreply.github.com>
2022-08-20 11:06:03 -0400

    Add files via upload

---
+++ Rasika Task - 3.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Radius if a circle = 8 
+to find:
+diameter of a circle = 2*8
+Circumference of a circle = 2*3.14*8
+Area of a circle = 3.14*8**
+
+
+# In[17]:
+
+
+radius=input("radius of a circle : ")
+
+
+# In[21]:
+
+
+#int
+
+r=8
+Pi=3.14
+
+#diameter:
+print(int(r*Pi))
+
+#Circumference:
+print(int(2*Pi*r))
+
+#Area:
+print(int(Pi*r*r))
+
+
+# In[22]:
+
+
+#float
+
+r=8
+Pi=3.14
+
+#diameter:
+print(float(r*Pi))
+
+#Circumference:
+print(float(2*Pi*r))
+
+#Area:
+print(float(Pi*r*r))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
