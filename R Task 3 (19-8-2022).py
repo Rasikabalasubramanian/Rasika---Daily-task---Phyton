85ceba06701691a3e6dcc2a9e35698875cdda125
Rasikabalasubramanian <110782682+Rasikabalasubramanian@users.noreply.github.com>
2022-08-20 14:23:55 -0400

    Add files via upload

---
+++ R Task 3 (19-8-2022).py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Radius 0f a circle = 8 
+#to find:
+#diameter of a circle = 2*8
+#Circumference of a circle = 2*3.14*8
+#Area of a circle = 3.14*8**
+
+
+# In[30]:
+
+
+#int
+
+r=int(input("radius of a circle : "))
+Pi=3.14
+
+#diameter:
+print(int(r*Pi))
+print(float(r*Pi))
+
+#Circumference:
+print(int(2*Pi*r))
+print(float(2*Pi*r))
+
+#Area:
+print(int(Pi*r*r))
+print(float(Pi*r*r))
+
+
+# In[32]:
+
+
+r = int(input("radius of a circle : "))
+Pi = 3.14
+
+#Diameter
+diameter = (2*r)
+print(diameter)
+print(type(diameter))
+
+#Circumference
+circumference = (2*Pi*r)
+print(circumference)
+print(type(circumference))
+
+#Area
+area = (Pi*r*r)
+print(area)
+print(type(area))
+
+
+# In[35]:
+
+
+r = int(input("radius of a circle : "))
+Pi = int(3.14)
+
+#Diameter
+diameter = (2*r)
+print(diameter)
+print(type(diameter))
+
+#Circumference
+circumference = (2*Pi*r)
+print(circumference)
+print(type(circumference))
+
+#Area
+area = (Pi*r*r)
+print(area)
+print(type(area))
+
+
+# In[ ]:
+
+
+
+
